73c4372f6cc5768d51d715fb044b3c9c31176544
Vasu Bhatia <vasubhatia163@gmail.com>
2025-11-03 00:37:19 +0530

    Fixed minor issues

--- frontend/src/api/chat.ts
+++ frontend/src/api/chat.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const isDev = import.meta.env.DEV;
 
 export const BASE_URL = isDev
-  ? "http://localhost:8000/api/v1/chat"
+  ? "http://localhost:8000/api/v1"
   : import.meta.env.VITE_API_URL;
 export const sendMessage = async (prompt: string, sessionId: string) => {
   const res = await axios.post(`${BASE_URL}/chat/reply`, { prompt, session_id: sessionId });
--- frontend/src/api/voice.ts
+++ frontend/src/api/voice.ts
@@ -2,9 +2,10 @@ import axios from "axios";
 const isDev = import.meta.env.DEV;
 console.log(isDev);
 export const BASE_URL = isDev
-  ? "http://localhost:8000/api/v1/chat"
+  ? "http://localhost:8000/api/v1"
   : import.meta.env.VITE_API_URL;
 
+console.log(`Endpoint: ${BASE_URL}/voice/process`)
 export const processVoice = async (audioBase64: string, sessionId: string) => {
   const res = await axios.post(`${BASE_URL}/voice/process`, { audio_base64: audioBase64, session_id: sessionId });
   return res.data;
