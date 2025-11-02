2698bc1971d9ed381da2c5759bde5aef8d248d82
Vasu Bhatia <vasubhatia163@gmail.com>
2025-11-03 00:06:37 +0530

    Added functionality to seperate production environment and development environment

--- frontend/src/api/chat.ts
+++ frontend/src/api/chat.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
+const isDev = import.meta.env.DEV;
 
-const BASE_URL = "http://localhost:8000/api/v1/chat";
-
+export const BASE_URL = isDev
+  ? "http://localhost:8000/api/v1/chat"
+  : import.meta.env.VITE_API_URL;
 export const sendMessage = async (prompt: string, sessionId: string) => {
   const res = await axios.post(`${BASE_URL}/reply`, { prompt, session_id: sessionId });
   return res.data;
--- frontend/src/api/voice.ts
+++ frontend/src/api/voice.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
-
-const BASE_URL = "http://localhost:8000/api/v1/voice";
+const isDev = import.meta.env.DEV;
+console.log(isDev);
+export const BASE_URL = isDev
+  ? "http://localhost:8000/api/v1/chat"
+  : import.meta.env.VITE_API_URL;
 
 export const processVoice = async (audioBase64: string, sessionId: string) => {
   const res = await axios.post(`${BASE_URL}/process`, { audio_base64: audioBase64, session_id: sessionId });
